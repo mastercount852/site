fca5f9c01441139b04aa5a34b66c2db623962d5b
SergioArrudaEquipeOuro <equipeouro85@gmail.com>
2025-07-25 20:50:12 -0300

    att

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,6 +11,14 @@ import { Painel04Component } from './components/painel04/painel04.component';
 import { Painel00Component } from './components/painel00/painel00.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { Painel05Component } from './components/painel05/painel05.component';
+import { HttpClient, HttpClientModule } from '@angular/common/http';
+import { TranslateHttpLoader } from '@ngx-translate/http-loader';
+import { TranslateModule, TranslateLoader } from '@ngx-translate/core';
+import { FooterComponent } from './components/footer/footer.component';
+
+export function HttpLoaderFactory(http: HttpClient) {
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
+}
 
 @NgModule({
   declarations: [
@@ -22,11 +30,21 @@ import { Painel05Component } from './components/painel05/painel05.component';
     Painel04Component,
     Painel00Component,
     NavbarComponent,
-    Painel05Component
+    Painel05Component,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TranslateModule.forRoot({
+      loader: {
+        provide:    TranslateLoader,
+        useFactory: HttpLoaderFactory,
+        deps:       [HttpClient]
+      },
+      defaultLanguage: 'en'
+    })
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { ScrollService } from 'src/app/services/scroll-service.service';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-navbar',
@@ -8,8 +9,23 @@ import { ScrollService } from 'src/app/services/scroll-service.service';
 })
 export class NavbarComponent implements OnInit {
   isMenuOpen = false;
+  currentLang!: string;
+  public dropdownOpen = false;
 
-  constructor(private scrollSrv: ScrollService) { }
+  
+  constructor(private scrollSrv: ScrollService, private translate: TranslateService) {
+    this.translate.addLangs(this.languages.map(lang => lang.code));
+
+    const savedLang = localStorage.getItem('language');
+    if (savedLang && this.languages.some(lang => lang.code === savedLang)) {
+      this.currentLang = savedLang;
+    } else {
+      this.currentLang = 'en'; // inglês como padrão
+    }
+
+    this.translate.setDefaultLang('en'); // sempre define inglês como padrão
+    this.translate.use(this.currentLang);
+  }
 
   ngOnInit(): void { }
 
@@ -23,5 +39,36 @@ export class NavbarComponent implements OnInit {
     // rolagem suave
     document.getElementById(id)
       ?.scrollIntoView({ behavior: 'smooth' });
+  }
+
+  languages = [
+    { code: 'en', name: 'English', flag: 'https://flagcdn.com/w40/gb.png' },
+    { code: 'pt', name: 'Português', flag: 'https://flagcdn.com/w40/br.png' },
+    { code: 'es', name: 'Español', flag: 'https://flagcdn.com/w40/es.png' },
+    { code: 'fr', name: 'Français', flag: 'https://flagcdn.com/w40/fr.png' },
+    { code: 'de', name: 'Deutsch', flag: 'https://flagcdn.com/w40/de.png' }
+  ];
+
+  toggleDropdown() {
+    this.dropdownOpen = !this.dropdownOpen;
+  }
+
+  switchLanguage(language: string) {
+    if (this.languages.some(lang => lang.code === language)) {
+      this.translate.use(language);
+      localStorage.setItem('language', language);
+      this.currentLang = language;
+      this.dropdownOpen = false;
+    }
+  }
+
+  getFlag(langCode: string): string {
+    const lang = this.languages.find(l => l.code === langCode);
+    return lang ? lang.flag : '';
+  }
+
+  getLanguageName(langCode: string): string {
+    const lang = this.languages.find(l => l.code === langCode);
+    return lang ? lang.name : '';
   }
 }
